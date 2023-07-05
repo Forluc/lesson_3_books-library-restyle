8f35b470d2da59a576b048e4e887cfdbc215b752
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-05 12:12:08 +0300

    Create README.md and update
    moved download_txt func to all_func
    add redirect protection

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-import requests
-import os
-from pathvalidate import sanitize_filename
-from all_func import parse_book_page
+from all_func import parse_book_page, download_txt
 import argparse
-
-
-def download_txt(url, filename, folder='books/'):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-    return filepath
 
 
 def get_book_data(book_number):
     book_data = parse_book_page(book_number)
-    download_txt(book_data['url'], book_data['title'])
-    return book_data['title'], book_data['author']
+    if book_data:
+        download_txt(book_data['url'], book_data['title'])
+        return book_data['title'], book_data['author']
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start_id', help='Начало скачивания книг по ID', default=1)
-    parser.add_argument('--end_id', help='Конец скачивания книг по ID', default=10)
+    parser.add_argument('--start_id', help='Начало скачивания книг по ID',
+                        default=1)  # Изменить default на нужное начало скачивания книг
+    parser.add_argument('--end_id', help='Конец скачивания книг по ID',
+                        default=10)  # Изменить default на нужный конец скачивания книг
     args = parser.parse_args()
 
     for book_number in range(int(args.start_id), int(args.end_id) + 1):
@@ -34,7 +22,7 @@ def main():
             title, author = get_book_data(book_number)
             print(f'{title}\n{author}')
         except:
-            print(f'Книги номер {book_number} не существует')
+            print(f'Книги номер {book_number} не существует, либо нет ссылки на скачивание')
         print()
 
 

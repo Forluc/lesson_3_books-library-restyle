433f787e9d994bc7ce74afd9e80d9353191c0547
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-07 23:10:54 +0300

    Update
    - remove redundant comments
    - rename func and variable names
    - add error outcomes(try-except) to main(TypeError, MissingSchema, ConnectionError,HTTPError)
    - add redirect check to func(download_txt)
    - add func download image
    - add a few small changes

--- all_func.py
+++ all_func.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit
 
 from bs4 import BeautifulSoup
 import lxml
@@ -7,75 +7,72 @@ import requests
 from pathvalidate import sanitize_filename
 
 
-def parse_book_page(book_number):
-    book_data = {
-        'title': None,
-        'author': None,
-        'picture_url': None,
-        'url': None,
-        'text': None,
-        'comments': None,
-        'genres': None,
-    }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    }
-    base_url = 'https://tululu.org'
-    book_data_url = urljoin(base_url, f'b{book_number}/')
-    response = requests.get(book_data_url, headers=headers, allow_redirects=True)
-    response.raise_for_status()
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
 
+    title_and_author = soup.find('table', class_='tabs').find('div', id='content').find('h1')
+    separator = title_and_author.text.find('::')
+    title = title_and_author.text[:separator].strip()
+    author = title_and_author.text[separator + len('::'):].strip()
+
+    picture = soup.find('body').find('div', class_='bookimage').find('img').get('src')
+    picture_url = urljoin(response.url, picture)
+
+    url_split = urlsplit(picture_url)
+    picture_path = url_split.path
+    slash_find = picture_path[1:].find('/')
+    picture_name = picture_path[slash_find + 2:]
+
+    links = soup.find('div', id='content').find('table').find_all('a')
+    for link in links:
+        if link.get('href')[:4] == '/txt':
+            url = urljoin(response.url, link.get('href'))
+            break
+        else:
+            url = None
+
+    text = soup.find('td', class_='ow_px_td').find_all('table', class_='d_book')[1].text
+
+    comments = []
+    for comment in soup.find('td', class_='ow_px_td').find_all('div', class_='texts'):
+        author_and_comment = comment.getText()
+        start_comment = author_and_comment.find(')')
+        commentary = author_and_comment[start_comment + len(')'):]
+        comments.append(commentary)
+
+    genres = []
+    for genre in soup.find('span', class_='d_book').find_all('a'):
+        genres.append(genre.getText())
+
+    book = {
+        'title': title,
+        'author': author,
+        'picture_url': picture_url,
+        'url': url,
+        'text': text,
+        'comments': comments,
+        'genres': genres,
+        'picture_name': picture_name,
+    }
+    return book
+
+
+def download_txt(url, filename, folder='books'):
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
     if not response.history:
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        #  -----------------------------------------title and author-----------------------------------------
-        title_and_author = soup.find('table', class_='tabs').find('div', id='content').find('h1')
-        separator = title_and_author.text.find('::')
-        book_data['title'] = title_and_author.text[:separator].strip()
-        book_data['author'] = title_and_author.text[separator + len('::'):].strip()
-
-        #  -----------------------------------------picture_url----------------------------------------------
-        picture = soup.find('body').find('div', class_='bookimage').find('img').get('src')
-        book_data['picture_url'] = urljoin(base_url, picture)
-
-        #  -----------------------------------------url------------------------------------------------------
-        try:
-            links = soup.find('div', id='content').find('table').find_all('a')
-        except:
-            links = None
-        for link in links:
-            if link.get('href')[:4] == '/txt':
-                book_data['url'] = urljoin(base_url, link.get('href'))
-                break
-
-        #  -----------------------------------------text-----------------------------------------------------
-        book_data['text'] = soup.find('td', class_='ow_px_td').find_all('table', class_='d_book')[1].text
-
-        #  -----------------------------------------comments-------------------------------------------------
-        comments = []
-        for comment in soup.find('td', class_='ow_px_td').find_all('div', class_='texts'):
-            author_and_comment = comment.getText()
-            start_comment = author_and_comment.find(')')
-            commentary = author_and_comment[start_comment + len(')'):]
-            comments.append(commentary)
-        book_data['comments'] = comments
-
-        #  -----------------------------------------genres---------------------------------------------------
-        genres = []
-        for genre in soup.find('span', class_='d_book').find_all('a'):
-            genres.append(genre.getText())
-        book_data['genres'] = genres
-
-        return book_data
+        os.makedirs(folder, exist_ok=True)
+        filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+        return filepath
 
 
-def download_txt(url, filename, folder='books/'):
-    response = requests.get(url)
+def download_image(url, filename, folder='images'):
+    response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
-
-    filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-    return filepath
+    if not response.history:
+        os.makedirs(folder, exist_ok=True)
+        filepath = os.path.join(folder, filename)
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
--- main.py
+++ main.py
@@ -1,28 +1,53 @@
-from all_func import parse_book_page, download_txt
+import time
+from urllib.parse import urljoin
+
+import requests
+
+from all_func import parse_book_page, download_txt, download_image
 import argparse
 
 
-def get_book_data(book_number):
-    book_data = parse_book_page(book_number)
-    if book_data:
-        download_txt(book_data['url'], book_data['title'])
-        return book_data['title'], book_data['author']
+def get_book(book_number):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    }
+    base_url = 'https://tululu.org'
+    book_data_url = urljoin(base_url, f'b{book_number}/')
+    response = requests.get(book_data_url, headers=headers, allow_redirects=True)
+    response.raise_for_status()
+
+    if not response.history:
+        book = parse_book_page(response)
+        if book:
+            download_txt(book['url'], book['title'])
+            download_image(book['picture_url'], book['picture_name'])
+            return book['title'], book['author']
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start_id', help='Начало скачивания книг по ID',
-                        default=1)  # Изменить default на нужное начало скачивания книг
-    parser.add_argument('--end_id', help='Конец скачивания книг по ID',
-                        default=10)  # Изменить default на нужный конец скачивания книг
+    parser.add_argument('--start_id', help='Начало скачивания книг по ID', default=1, type=int)
+    parser.add_argument('--end_id', help='Конец скачивания книг по ID', default=10, type=int)
     args = parser.parse_args()
 
-    for book_number in range(int(args.start_id), int(args.end_id) + 1):
+    books_count = args.end_id - args.start_id
+    counter = 0
+
+    while books_count >= counter:
+        book_number = args.start_id + counter
         try:
-            title, author = get_book_data(book_number)
-            print(f'{title}\n{author}')
-        except:
-            print(f'Книги номер {book_number} не существует, либо нет ссылки на скачивание')
+            title, author = get_book(book_number)
+            print(f'Книга номер {book_number}\n{title}\n{author}')
+            counter += 1
+        except (TypeError, requests.exceptions.MissingSchema) as ex:
+            print(f'Книга номер {book_number}\nERROR: {ex}')
+            counter += 1
+        except requests.exceptions.ConnectionError as ex:
+            print(f'Книга номер {book_number}\nERROR: {ex}')
+            time.sleep(5)
+        except requests.exceptions.HTTPError as ex:
+            print(f'Книга номер {book_number}\nERROR: {ex}')
+            counter += 1
         print()
 
 
